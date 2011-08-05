2de9c5c961f43b2acd8f7dc3bb00d97b31263b43
Mike Skalnik <mike.skalnik@gmail.com>
2011-08-05 18:44:19 -0400

    Catch any errors from auto-finding of DOM ID and prompt if not found

--- source.js
+++ source.js
@@ -4,7 +4,17 @@ var awesome = function(domId) {
   evt.initMouseEvent("click", true, true, window,1, 0, 0, elem.offset().left, elem.offset().top, false, false, false, false, 0, null);
   document.getElementById(domId).dispatchEvent(evt)
 };
-awesome.domId = $('a[style*=left\\:\\ 370px]')[0].id;
+
+try {
+  awesome.domId = $('a[style*=left\\:\\ 370px]')[0].id;
+} catch(e) {
+  console.log("Could not automatically detect awesome button's DOM ID. Caught error: " + e);
+}
+
+if(typeof awesome.domId == 'undefined') {
+  awesome.domId = window.prompt("What's your awesome button's DOM ID?", "");
+}
+
 setInterval('awesome("'+awesome.domId+'")', 45000);
 awesome(awesome.domId);
 window.alert("I don't always awesome, but when I do, I do it automatically.")
