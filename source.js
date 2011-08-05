34f8476dd89e3ba1a220f1bae7b116972bf2ea22
Mike Skalnik <mike.skalnik@gmail.com>
2011-08-05 01:22:52 -0400

    Lookup DOM ID automatically with styling.

--- source.js
+++ source.js
@@ -4,5 +4,6 @@ var awesome = function(domId) {
   evt.initMouseEvent("click", true, true, window,1, 0, 0, elem.offset().left, elem.offset().top, false, false, false, false, 0, null);
   document.getElementById(domId).dispatchEvent(evt)
 };
-awesome.domId = window.prompt("What's your awesome button's DOM ID?", "");
-setInterval('awesome("'+awesome.domId+'")', 45000)
+awesome.domId = $('a[style*=left\\:\\ 370px]')[0].id;
+setInterval('awesome("'+awesome.domId+'")', 45000);
+window.alert("I don't always awesome, but when I do, I do it automatically.")
